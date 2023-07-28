2c007633ac6ef954cbbe7efddd5a31a5a7f9df0f
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-28 13:31:15 -0300

    Name, Course, Enrollment, Shift and Status being inserted without Bugs

--- src/main/java/br/com/academy/controllers/AlunoController.java
+++ src/main/java/br/com/academy/controllers/AlunoController.java
@@ -1,6 +1,8 @@
 package br.com.academy.controllers;
 
 import br.com.academy.model.Aluno;
+import br.com.academy.dao.AlunoDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AlunoController {
+
+    @Autowired
+    private  AlunoDao alunorepositorio;
 
     @GetMapping("/inserindoAlunos")
     public ModelAndView inserindoAluno(Aluno aluno){
@@ -20,6 +25,7 @@ public class AlunoController {
     public ModelAndView inserirAluno(Aluno aluno){
         ModelAndView mv = new ModelAndView();
         mv.setViewName("redirect:/Aluno/lisAlunos");
+        alunorepositorio.save(aluno);
         return mv;
     }
 }
--- src/main/java/dao/AlunoDao.java
+++
@@ -1,5 +0,0 @@
-package dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface AlunoDao {
-}
